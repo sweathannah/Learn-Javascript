4a8a0230577641c3e044a4cfb10530af6cb2ae20
unknown <sultanatmbashir@gmail.com>
2024-01-24 12:31:46 +0100

    class-countinues

--- class.js
+++ class.js
@@ -145,9 +145,139 @@ let num = 20;
     num--;
     console.log(num);
 
-                        // Ternary Operators
+
+                    // symbols
+const mySymbol = Symbol();
+console.log(mySymbol);
+const newSymbol = ("hi");
+console.log(newSymbol);
+
+
+                        // concatination
+const love = "I love";
+const luv = love[2];
+console.log(luv);
+console.log(love.replace("love", "hate"))
+const sentence = love + " " + "studing " + day;
+console.log(sentence);
+
+                    
+const backtickSentence = `${love}  studying  ${day}`;
+console.log(backtickSentence);
+
+const myArray = ["Rukaya", "Marvellous", "Grace", "Munirah"];
+console.log(myArray);
+myArray[0] = "Salamat";
+console.log(myArray);
+
+
+                    // math object
+const four = 4;
+console.log(Math.sqrt(four));
+console.log(Math.pow(four, 2));
+console.log(Math.PI);
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+console.log(Math.max(3,4,9,0));
+
+// trying to get a random number range 0-10 which allows float/decimal numbers
+// console.log(Math.floor(Math.random()*10 +1)) //this allows only integers
+let random = (Math.random()*10 + 1);
+const click = random;
+
+
+                    //window operators
+
+// let greetings = "Hello";
+// alert(`${greetings}`);      
+
+// let name = prompt("Enter Your name");
+// console.log(`${name}`);
+
+// let firstNumber = prompt("Enter number");//prompt are naturally strings
+// let secondNumber = prompt("Enter number");
+// // let output = firstNumber + secondNumber; here the number is seen as strings to make the browser see it has number its done in three ways
+// // let output = Number(firstNumber) + Number(secondNumber);  //first method
+// // let output = +firstNumber + +secondNumber; //second method
+// console.log(output);
+
+const agree = confirm("Are you sure you want to subscribe");
+console.log(agree);
+
+//Regex
+    let numBer = "I love Javascript and i belive you also love javascript";
+    let res = /javascript/gi;
+    console.log(numBer.match(res))
+
+    const sentenc = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+    let inequalities = /[@#$%&;]/g;
+    console.log (sentenc.replace (inequalities, ""));
+
+    let extract = "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month";
+    let extractNum = /\d+/gi;
+    let a = extract.match(extractNum);
+    console.log(a);
+    let ai = +a[0] * 12;
+    console.log(ai);
+    let bi = +a[2] * 12;
+    console.log(bi)
+    let output = ai + bi + 10000;
+    console.log(output);
+    
+
+                            // Ternary Operators
+
+// let isAMan = true;
+// isAMan ? console.log("yes, he is a man") :
+// console.log ("No, he is not a man");
 
 let isAMan = true;
-isAMan ? console.log("yes, he is a man") :
-console.log ("No, he is not a man");
+if (isAMan){
+    console.log('yes, he is a man');
+}
+else{
+    console.log("No, he is not a man");
+}
+
+let b = 5;
+if (b < 5){
+    console.log('Not correct') 
+}
+else if (b = 5){
+    console.log("It's correct");
+}
+else if (b >5){
+    console.log("It's wrong")
+}
+else{
+    console.log("Try again");
+}
+
+
+let today = prompt('What day is today');
+let days = today.toLocaleLowerCase()
+switch (days) {
+    case 'monday':
+        console.log('Today is Monday')
+    break;
+    case 'tuesday':
+        console.log('Today is tuesday')
+    break;
+    case 'wednesday':
+        console.log('Today is Wednesday')
+    break;
+    case 'thursday':
+        console.log('Today is Thursday')
+    break;
+    case 'friday':
+        console.log('Today is Friday')
+    break;
+    default:
+        console.log('It is not a day')
+}
+
 
